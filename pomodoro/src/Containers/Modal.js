2e1a140a7b24fcbfac420f5347459d71e838d2bc
Will Bainton <baintonw@gmail.com>
2020-06-22 17:18:50 -0400

    modal styling

--- pomodoro/src/Containers/Modal.js
+++ pomodoro/src/Containers/Modal.js
@@ -7,7 +7,7 @@ const Modal = (props) => {
     return(
         <div className="modal-container">
             <div className="modal-container__content">
-                <h2 className="modal-heading">What are you working on today?</h2>
+                <h2 className="modal-container__content__heading">What are you working on today?</h2>
                 <form>
                     {/* <label>What are you working on today?</label> */}
                 </form>
