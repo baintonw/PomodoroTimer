dc11986df75ffec2c91cf1e6f6a7820e3d7cf309
Will Bainton <baintonw@gmail.com>
2020-08-14 17:31:50 -0400

    set/timesheet styling edits

--- pomodoro/src/Components/Timesheet.js
+++ pomodoro/src/Components/Timesheet.js
@@ -3,15 +3,17 @@ import React from 'react'
 //Components
 import Set from './Set'
 
+const colorArray = [];
+
 const Timesheet = (props) => {
     return (
         <div className="timesheet">
+            <h1>Timesheet - 08/14/20</h1>
             <Set number="1"></Set>
             <Set number="2"></Set>
             <Set number="3"></Set>
             <Set number="4"></Set>
             <Set number="5"></Set>
-
         </div>
     )
 }
